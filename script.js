dc7fa732ffe27c06e3b5e3ae6953f1cced107055
Md Nazmul Huda Rimon <nhr.developer@gmail.com>
2021-02-08 22:48:51 +0600

    done

--- script.js
+++ script.js
@@ -1,72 +1,58 @@
-const searchBtn = document.getElementById('searchButton');
-const meals = document.getElementById('meal');
-const mealDetails = document.querySelector('.meal-details-content');
-const closeBtn = document.getElementById('recipe-close-btn');
-
-// event listeners
-searchBtn.addEventListener('click', mealsList);
-meals.addEventListener('click', mealRecipe);
-closeBtn.addEventListener('click', () => {
-    mealDetails.parentElement.classList.remove('showRecipe');
-});
-
-
-// get meal list that matches with the ingredients
-function mealsList(){
-    let searchInputTxt = document.getElementById('input').value.trim();
-    fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${searchInputTxt}`)
-    .then(response => response.json())
+function searchMeals(){
+    const searchInput = document.getElementById('search-field').value;
+    const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchInput}`;
+    fetch(url)
+    .then(res => res.json())
     .then(data => {
-        let html = "";
-        if(data.meals){
-            data.meals.forEach(meal => {
-                html += `
-                    <div class = "meal-item" data-id = "${meal.idMeal}">
-                        <div class = "meal-img">
-                            <img src = "${meal.strMealThumb}" alt = "food">
-                        </div>
-                        <div class = "meal-name">
-                            <h3>${meal.strMeal}</h3>
-                            <a href = "#" class = "recipe-btn">Get Recipe</a>
-                        </div>
-                    </div>
-                `;
-            });
-            meals.classList.remove('notFound');
-        } else{
-            html = "Sorry, we didn't find any meal!";
-            meals.classList.add('notFound');
-        }
+        // console.log(data);
+        getMeals(data.meals);
+    })
+}
 
-        meals.innerHTML = html;
+
+const getMeals = meals =>{
+    const mealContainer = document.getElementById('meal-list');
+    mealContainer.innerHTML ='';
+    meals.forEach(meal=>{
+        const mealDiv = document.createElement('div');
+        mealDiv.className = 'single-result row justify-content-center m-2 p-3 spacing';
+        mealDiv.innerHTML = `
+        <div class = "meal" onclick ="details('${meal.idMeal}')">
+            <img src = "${meal.strMealThumb}" alt = "food" class = "img-fluid">
+            <h3 class = "mt-3">${meal.strMeal}</h3>
+            </div>
+        </div>`;
+        mealContainer.appendChild(mealDiv);
     });
-}
-function mealRecipe(e){
-    e.preventDefault();
-    if(e.target.classList.contains('recipe-btn')){
-        let mealItem = e.target.parentElement.parentElement;
-        fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealItem.dataset.id}`)
-        .then(response => response.json())
-        .then(data => modal(data.meals));
-    }
+};
+
+const details =(mealNameId) =>{
+    const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealNameId}`;
+    fetch(url)
+    .then(res =>res.json())
+    .then(data =>{
+        getDetails(data.meals);
+    })
 }
 
-// create a modal
-function modal(meal){
-    console.log(meal);
-    meal = meal[0];
-    let html = `
-        <h2 class = "recipe-title">${meal.strMeal}</h2>
-        <p class = "recipe-category">${meal.strCategory}</p>
-        <div class = "recipe-instruct">
-            <h3>Instructions:</h3>
-            <p>${meal.strInstructions}</p>
-        </div>
-        <div class = "recipe-meal-img">
-            <img src = "${meal.strMealThumb}" alt = "">
-        </div>
-       
-    `;
-    mealDetails.innerHTML = html;
-    mealDetails.parentElement.classList.add('showRecipe');
+
+function getDetails(meal1){
+    const mealDetails = document.getElementById('meal-details');
+    mealDetails.innerHTML ='';
+    meal1.forEach(mealDetail=>{
+       const detailsDiv = document.createElement('div');
+       detailsDiv.className = 'col-md-6 mx-auto';
+       detailsDiv.innerHTML = `
+       <img src="${mealDetail.strMealThumb}" class= "img-fluid">
+       <h3 class = " mt-3">${mealDetail.strMeal}</h3>
+       <ul>
+       <li>${mealDetail.strIngredient1}</li>
+       <li>${mealDetail.strIngredient2}</li>
+       <li>${mealDetail.strIngredient3}</li>
+       <li>${mealDetail.strIngredient4}</li>
+       </ul>
+      
+       `;
+       mealDetails.appendChild(detailsDiv);
+    })
 }
